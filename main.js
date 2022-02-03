bc4793ec4fe61ae4e7ed7e73317d2634ac754b7d
Paulo Rubens <paulorubenspereira@gmail.com>
2022-02-03 17:14:02 -0300

    create component without CSS

--- main.js
+++ main.js
@@ -2,9 +2,18 @@ import './src/Styles/settings/colors.css';
 import './src/Styles/generic/generic.css';
 import './src/Styles/elements/base.css';
 import BoardGame from "./src/objects/BoardGame";
+import PlayerName from './src/components/PlayerName';
 
 
 const $root = document.querySelector('#root');
-const $htmlBoardGame = BoardGame(6);
 
-$root.insertAdjacentHTML("beforeend", $htmlBoardGame);
+$root.insertAdjacentHTML(
+    "beforeend",
+    `
+        ${PlayerName('Player 01')}
+        ${PlayerName('Player 02')}
+        ${BoardGame(6)}
+
+        `
+
+)
