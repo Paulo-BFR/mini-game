f252f5ffe14027aa768b833078dbf1daeea15417
Paulo Rubens <paulorubenspereira@gmail.com>
2022-02-05 12:17:10 -0300

    click event in CardFrontBack

--- src/components/cardGame/index.js
+++ src/components/cardGame/index.js
@@ -1,11 +1,12 @@
-import './style.css';
 
-function CardGame(icon = "alura-pixel", alt = 'logo alura') {
-    return  /*html*/`
+import "./style.css";
+
+function CardGame(icon = "alura-pixel", alt = "Logo da Alura") {
+    return /*html*/ `
     <article class="card-game">
-        <img src="images/${icon}.png" alt='${alt}'>
+      <img src="images/${icon}.png" alt="${alt}">
     </article>
-    `;
+  `;
 }
 
 export default CardGame;
--- src/objects/BoardGame/index.js
+++ src/objects/BoardGame/index.js
@@ -1,17 +1,15 @@
-import './style.css'
-import CardGame from "../../components/cardGame";
-import CardFrontBack from '../../components/CardFrontBack';
-
+import "./style.css";
+import CardFrontBack from "../../components/CardFrontBack";
 
 function BoardGame(amountCards) {
     const $htmlCardFrontBack = CardFrontBack();
-    const $htmlContent = $htmlCardFrontBack.repeat(amountCards)
+    const $htmlContent = $htmlCardFrontBack.repeat(amountCards);
 
     return /*html*/ `
     <section class="board-game">
-       ${$htmlContent}
+      ${$htmlContent}
     </section>
-    `;
+  `;
 }
 
 export default BoardGame;
